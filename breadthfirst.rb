87ee710c3f66781c421ad5f424859d4a0f9e868b
Matt <matthewmorgante@gmail.com>
2015-04-03 17:39:13 +0700

    depthfirst

---
+++ breadthfirst.rb
@@ -0,0 +1,13 @@
+# breadthfirst.rb
+
+# find the shortest path between nodes in a graph 
+# filesystem traversal 
+# printing heirarchies in rank order 
+
+# breadth first nodes within a tree = go down a row, 
+# traverse the nodes from left to right 
+
+# depth first search
+# swap the queue across with a stack 
+# stack goes downwards first 
+
