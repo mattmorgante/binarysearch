d30d4825f50bf15ad6ec465e74c65f358c42cabd
Matt <matthewmorgante@gmail.com>
2015-04-03 14:44:00 +0700

    project specs'

---
+++ node.rb
@@ -0,0 +1,24 @@
+class Node 
+  #should have a value that it stores
+  # should link to parents and children 
+
+  def build_tree array
+    # should take unstructured data and turn it into a binary tree 
+    # tree of Node objects 
+    # is it a leaf? or is it a branch in the middle somewhere
+  end 
+
+  def breadth_first_search 
+    #takes a target value and returns the node 
+  end 
+
+
+  def depth_first_search 
+    # returns the node at which the target value is located 
+  end 
+
+  def dfs_rec 
+    # runs a depth first search recursively 
+    # little robot running down the tree each time 
+  end 
+end 
